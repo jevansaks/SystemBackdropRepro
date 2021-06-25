6c1b1ab59105c1b09ec3a925ceea45da61efb7fc
Jevan Saks <jevansa@microsoft.com>
2021-06-24 20:39:53 -0700

    Add repro

---
+++ SystemBackdropRepro/MainPage.cpp
@@ -0,0 +1,52 @@
+﻿#include "pch.h"
+#include "MainPage.h"
+#include "MainPage.g.cpp"
+#include "winrt\windows.ui.h"
+#include "winrt\windows.ui.composition.h"
+
+using namespace winrt;
+using namespace Windows::UI::Xaml;
+
+namespace winrt::SystemBackdropRepro::implementation
+{
+    MainPage::MainPage()
+    {
+        InitializeComponent();
+
+    }
+
+    winrt::Windows::Foundation::Size MainPage::MeasureOverride(winrt::Windows::Foundation::Size const& availableSize)
+    {
+        ::Sleep(250);
+        return __super::MeasureOverride(availableSize);
+    }
+
+
+    int32_t MainPage::MyProperty()
+    {
+        throw hresult_not_implemented();
+    }
+
+    void MainPage::MyProperty(int32_t /* value */)
+    {
+        throw hresult_not_implemented();
+    }
+
+    void MainPage::ClickHandler(IInspectable const&, RoutedEventArgs const&)
+    {
+        auto backdrop = Window::Current().try_as<winrt::Windows::UI::Composition::ICompositionSupportsSystemBackdrop>();
+        if (backdrop.SystemBackdrop())
+        {
+            backdrop.SystemBackdrop(nullptr);
+            myButton().Content(box_value(L"Currently: XAML backdrop"));
+        }
+        else
+        {
+            myButton().Content(box_value(L"Currently: System backdrop"));
+            auto compositor = Window::Current().Compositor();
+            auto brush = compositor.CreateColorBrush(Windows::UI::Color{ 255,0,0,0 });
+            backdrop.SystemBackdrop(brush);
+        }
+
+    }
+}
---
+++ SystemBackdropRepro/MainPage.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+
+#include "MainPage.g.h"
+
+namespace winrt::SystemBackdropRepro::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+
+        int32_t MyProperty();
+        void MyProperty(int32_t value);
+
+        winrt::Windows::Foundation::Size MeasureOverride(winrt::Windows::Foundation::Size const& availableSize);
+
+        void ClickHandler(Windows::Foundation::IInspectable const& sender, Windows::UI::Xaml::RoutedEventArgs const& args);
+    };
+}
+
+namespace winrt::SystemBackdropRepro::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
